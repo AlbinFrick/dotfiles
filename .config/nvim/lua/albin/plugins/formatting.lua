6478551f302f8b29bef27ed375ca28414c823c4b
Albin Frick <abbzez@gmail.com>
2024-04-22 22:52:20 +0200

    nvim: update and fix prettier

---
+++ .config/nvim/lua/albin/plugins/formatting.lua
@@ -0,0 +1,35 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "LspAttach", "BufReadPost", "BufNewFile" },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      scss = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      html = { "prettier" },
+      svelte = { "prettier" },
+    },
+    format_on_save = {
+      timeout_ms = 2500,
+      lsp_fallback = true,
+    },
+  },
+  config = function(_, opts)
+    local conform = require("conform")
+
+    -- Setup "conform.nvim" to work
+    conform.setup(opts)
+
+    -- Customise the default "prettier" command to format Markdown files as well
+    conform.formatters.prettier = {
+      prepend_args = { "--prose-wrap", "always" },
+    }
+  end,
+}
