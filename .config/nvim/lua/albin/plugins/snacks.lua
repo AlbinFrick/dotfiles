c9e263ed52b9c80e3a076ffcd0da0990fa654612
Albin Frick <abbzez@gmail.com>
2024-11-11 19:49:21 +0100

    nvim: remove unused options for snacks.nvim

--- .config/nvim/lua/albin/plugins/snacks.lua
+++ .config/nvim/lua/albin/plugins/snacks.lua
@@ -69,13 +69,6 @@ return {
       desc = "Lazygit Log (cwd)",
     },
     {
-      "<leader>cR",
-      function()
-        Snacks.rename()
-      end,
-      desc = "Rename File",
-    },
-    {
       "<c-/>",
       function()
         Snacks.terminal()
@@ -140,9 +133,7 @@ return {
         -- Create some toggle mappings
         Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
         Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
         Snacks.toggle.diagnostics():map("<leader>ud")
-        Snacks.toggle.line_number():map("<leader>ul")
         Snacks.toggle
           .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
           :map("<leader>uc")
