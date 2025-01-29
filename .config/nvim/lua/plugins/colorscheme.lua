5120fb71dfde56f67ac9487666c350081e4f39a6
Albin Frick <abbzez@gmail.com>
2025-01-29 16:00:55 +0100

    nvim: use default lazyVim theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,20 @@
 return {
   -- Add catppuccin
 
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup()
-    end,
-  },
-
-  --Configure LazyVim to load catppuccin
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   config = function()
+  --     require("catppuccin").setup()
+  --   end,
+  -- },
+  --
+  -- --Configure LazyVim to load catppuccin
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
 }
